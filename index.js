9d54868bd8f83494ed89e4f9aeecac604e178173
Daffa Fakhuddin Arrozy <daffatgi02@gmail.com>
2023-01-15 02:51:02 +0700

    Menambahkan Create + Read data

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 app.use(bodyParser.json());
 
+//Config DB
 const connection = mysql.createConnection({
   host: '66.118.234.39',
   user: 'u40_1fcVHWTpwn',
   password: '+qzCOyK9=bU6SoJOHzhvlQM!',
   database: 's40_daffa_db'
 });
-
 connection.connect();
 
-const app = express();
-const port = 3000;
+// Membuat data
+app.post('/users', (req, res) => {
+  const { nama, umur, agama } = req.body;
+  connection.query('INSERT INTO users (nama, umur, agama) VALUES (?, ?, ?)', [nama, umur, agama], (error, results) => {
+    if (error) throw error;
+    res.status(201).send(`User added with ID: ${results.insertId}`);
+  });
+});
 
+// Membaca data
 app.get('/users', (req, res) => {
   connection.query('SELECT * FROM users', (error, results) => {
     if (error) throw error;
