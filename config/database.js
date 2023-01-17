b612a41af1e3fbc5d4897bc0baa6f187ef7420ba
Daffa Fakhuddin Arrozy <daffatgi02@gmail.com>
2023-01-17 11:51:13 +0700

    Membuat Config DB

---
+++ config/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: '66.118.234.39',
+  user: 'u40_1fcVHWTpwn',
+  password: '+qzCOyK9=bU6SoJOHzhvlQM!',
+  database: 's40_daffa_db'
+});
+
+module.exports = connection;
